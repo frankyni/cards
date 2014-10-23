cf51f042d6d5e6aa90bf580a17bbec0ccc59da7f
frankyni <franky.chenxni@gmail.com>
2014-10-23 14:47:33 -0400

    cards delete remote

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,7 @@
 class CardsController < ApplicationController
 
+	before_action :set_card, only: [:update, :destroy]
+
 	def create
 		@card = Card.create(title: "new card", body: "write something here ...", project_id: params[:project_id])
 		redirect_to :back, notice: "New card added"
@@ -9,5 +11,17 @@ class CardsController < ApplicationController
 	end
 
 	def destroy
+		@card.destroy
+
+		respond_to do |format|
+			format.html {redirect_to :back, notice: "Card deleted"}
+			format.js
+		end
+	end
+
+	private
+
+	def set_card
+		@card = Card.find(params[:id])
 	end
 end
